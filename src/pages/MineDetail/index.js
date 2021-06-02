2420b0254051d24952fe1ec62a4eef215873cd28
litianao <astrorobbie@163.com>
2021-06-02 08:45:27 +0800

    remove false

--- src/pages/MineDetail/index.js
+++ src/pages/MineDetail/index.js
@@ -467,7 +467,6 @@ export default function MineDetail(props) {
               {isLocked ? (
                 <LockedButton />
               ) : wallet.status === "connected" ? (
-                false &&
                 approveParams &&
                 approveParams.txs &&
                 approveParams.txs.length > 0 ? (
