9b534bf29faf5370689f59ff9d3d525690f10c92
rocker <rocker@gmail.com>
2021-12-14 16:25:20 +0800

    added new nft api

--- src/utils/graph.js
+++ src/utils/graph.js
@@ -6,88 +6,107 @@ import axios from "axios";
 //   "https://subgraph-test.icarus.finance/subgraphs/name/ica/gamefi-subgraph-test";
 
 //prod
-const baseURL = 'https://subgraph-test.icarus.finance/subgraphs/name/ica/gamefi-mainnet'
+// const baseURL = 'https://subgraph-test.icarus.finance/subgraphs/name/ica/gamefi-mainnet'
+
+const baseURL = "https://nft-api.icarus.finance";
 
 const getBlindBox = async (owner) => {
   if (!owner) {
     return;
   }
-  const result = await axios.post(baseURL, {
-    query: `{
-          blindBoxes(orderBy: createdAt, orderByDirection: asc, where:{owner: "${owner.toLowerCase()}"} ){
-            id
-            staked
-            value
-            capsule
-            state
-            createdAt
-            endAt
-            medias
-          }
-        }`,
+  const result = await axios.get(`${baseURL}/api/box`, {
+    params: {
+      owner,
+      pageSize: 1000,
+    },
+    // query: `{
+    //       blindBoxes(orderBy: createdAt, orderByDirection: asc, where:{owner: "${owner.toLowerCase()}"} ){
+    //         id
+    //         staked
+    //         value
+    //         capsule
+    //         state
+    //         createdAt
+    //         endAt
+    //         medias
+    //       }
+    //     }`,
   });
-  result.data.data.blindBoxes.forEach((item) => {
-    item.createdAt = item.createdAt * 1000;
-    item.endAt = item.endAt * 1000;
+  console.log("result", result);
+  result.data.data.forEach((item) => {
+    item.createdAt = item.createdAtTimestamp * 1000;
+    item.endAt = item.endAtTimestamp * 1000;
     // item.isOld = item.createdAt < 1635901200000
   });
-  return result.data.data.blindBoxes;
+  return result.data.data;
 };
 
 const getCollection = async (owner) => {
   if (!owner) {
     return;
   }
-  const result = await axios.post(baseURL, {
-    query: `{
-      medias(first: 1000, where:{owner: "${owner.toLowerCase()}"}) {
-        id,
-        contentURI,
-      }
-    }`,
+  const result = await axios.get(`${baseURL}/api/media`, {
+    params: {
+      owner,
+      pageSize: 1000,
+    },
+    // query: `{
+    //   medias(first: 1000, where:{owner: "${owner.toLowerCase()}"}) {
+    //     id,
+    //     contentURI,
+    //   }
+    // }`,
   });
 
-  let medias = result.data.data.medias
+  let medias = result.data.data;
 
   for (let i = 0; i < medias.length; i++) {
-    let externalInfo = await getContentURI(medias[i].id);
-    if(!externalInfo){
-      continue
+    let externalInfo = (await axios.get(medias[i].contentURI)).data;
+
+    if (!externalInfo) {
+      continue;
     }
-    externalInfo.dropRate = (Number(externalInfo.attributes[0].drop_rate) / 100).toFixed(2)
-    externalInfo.contentURI = externalInfo.animation_url
+    externalInfo.dropRate = (
+      Number(externalInfo.attributes[0].drop_rate) / 100
+    ).toFixed(2);
+    externalInfo.contentURI = externalInfo.animation_url;
     medias[i] = {
       ...medias[i],
-      ...externalInfo
-    }
+      ...externalInfo,
+    };
   }
+
+  // for (let i = 0; i < medias.length; i++) {
+  //   let externalInfo = await getContentURI(medias[i].id);
+  //   if(!externalInfo){
+  //     continue
+  //   }
+  //   externalInfo.dropRate = (Number(externalInfo.attributes[0].drop_rate) / 100).toFixed(2)
+  //   externalInfo.contentURI = externalInfo.animation_url
+  //   medias[i] = {
+  //     ...medias[i],
+  //     ...externalInfo
+  //   }
+  // }
 
   return medias;
 };
 
 const getContentURI = async (tokenId) => {
-  const result = await axios.post(baseURL, {
-    query: `{
-      medias(where:{id: "${tokenId}"}) {
-        id,
-        contentURI
-      }
-    }`,
+  const result = await axios.get(`${baseURL}/api/media`, {
+    params: {
+      id: tokenId,
+    },
   });
-  if (
-    result.data.data.medias &&
-    result.data.data.medias.length > 0 &&
-    result.data.data.medias[0].contentURI.indexOf("ipfs.io") > -1
-  ) {
-    const jsonURI = result.data.data.medias[0].contentURI;
-    try {
-      const res = await axios.get(jsonURI);
-      return res.data;
-    } catch (err) {
-      console.log(err);
-    }
-  } else {
-    return null;
+
+  const contentURI = result.data.data[0].contentURI;
+
+  try {
+    const res = (await axios.get(contentURI)).data;
+    console.log(res, "content uri response");
+    return res;
+  } catch (err) {
+    console.log(err);
   }
 };
 
